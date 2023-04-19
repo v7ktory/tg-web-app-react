2876852340303fd50cb30c7aea9d92ce0fa17978
Victor <v7ktory@gmail.com>
2023-04-19 19:26:15 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './src/../hooks/useTelegram';
+import Header from './components/header/header';
 
 function App() {
   const {onToggleButton, tg} = useTelegram()
