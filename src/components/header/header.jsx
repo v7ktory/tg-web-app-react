d08c0e06e670a933cbbb1a290f0e40edfb304e48
Victor <v7ktory@gmail.com>
2023-04-19 19:30:26 +0700

    initial

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,9 +7,7 @@ const Header = () => {
     return (
         <div className={"header"}>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={"username"}>
-                {user?.username}
-            </span>
+            <span className={"username"}>{user?.username}</span>
         </div>
     )
 }
